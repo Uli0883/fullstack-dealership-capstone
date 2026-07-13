59d0aca227f043e9472f59b4b1c2f58bf5dc7400
Ulises Carrillo <156877334+Uli0883@users.noreply.github.com>
2026-07-13 15:29:39 +0000

    Agrego autenticación con Login/Logout

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', TemplateView.as_view(template_name='About.html'), name='about'),
     path('contact/', TemplateView.as_view(template_name='Contact.html'), name='contact'),
-    path('', TemplateView.as_view(template_name='Home.html'), name='home'),  # <-- Agrega esta línea
+    path('', TemplateView.as_view(template_name='Home.html'), name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
